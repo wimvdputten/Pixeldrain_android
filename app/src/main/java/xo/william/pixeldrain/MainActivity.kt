3da79c7592479d368ffd0ff7a7fd1a76cf6a1043
Unknown <wimvdput@gmail.com>
2020-08-19 10:59:38 +0200

    Added base toolbar

--- app/src/main/java/xo/william/pixeldrain/MainActivity.kt
+++ app/src/main/java/xo/william/pixeldrain/MainActivity.kt
@@ -2,10 +2,19 @@ package xo.william.pixeldrain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.main_toolbar));
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_toolbar, menu)
+        return true
     }
 }
