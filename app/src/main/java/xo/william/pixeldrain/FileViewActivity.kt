5afafe902bfb18964fc275058fcdd493b0c9d15d
williamXo <wimvdput@gmail.com>
2021-01-17 20:17:15 +0100

    feat: added exoplayer to play video files

--- app/src/main/java/xo/william/pixeldrain/FileViewActivity.kt
+++ app/src/main/java/xo/william/pixeldrain/FileViewActivity.kt
@@ -1,7 +1,5 @@
 package xo.william.pixeldrain
 
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -9,17 +7,26 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
 import kotlinx.android.synthetic.main.activity_file_view.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import xo.william.pixeldrain.fileList.InfoModel
+
 
 class FileViewActivity : AppCompatActivity() {
 
     private val format = Json { ignoreUnknownKeys = true }
 
     private lateinit var infoModel:InfoModel;
+
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,7 +52,11 @@ class FileViewActivity : AppCompatActivity() {
         if (type.contains("image")){
             loadImage();
         }
+        if (type.contains("video")){
+            loadVideo()
+        }
     }
+
     fun loadImage(){
         val imageFile = findViewById<ImageView>(R.id.imageFile)
         val fileProgress = findViewById<ProgressBar>(R.id.fileProgressBar);
@@ -60,7 +71,21 @@ class FileViewActivity : AppCompatActivity() {
             Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_LONG).show();
             Log.e("loadImage", "error: " + infoModel.getThumbnailUrl() + "  " + e.message)
         }
+    }
 
+    fun loadVideo(){
+        val fileProgress = findViewById<ProgressBar>(R.id.fileProgressBar)
+        fileProgress.visibility = View.GONE
+        val videoExoFile = findViewById<PlayerView>(R.id.videoExoFile)
+        val exoPlayer = SimpleExoPlayer.Builder(this).build()
+        videoExoFile.player = exoPlayer;
+
+        val mediaItem: MediaItem = MediaItem.fromUri(infoModel.getFileUrl())
+        exoPlayer.setMediaItem(mediaItem);
+        exoPlayer.prepare()
+
+
+        exoPlayer.play()
     }
 
 
